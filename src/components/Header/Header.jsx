4c4431be1a8fc42ff4a8bda0b4914993d225952f
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-31 21:11:33 +0530

    feat/added-logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink, useNavigate} from 'react-router-dom'
 import { TiThMenu } from "react-icons/ti";
 
 export default function Header() {
+
+    const navigate = useNavigate();
+    const logout = ()=>{
+        localStorage.removeItem("user");
+        navigate("/")
+    }
+
     return (
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
@@ -16,13 +23,14 @@ export default function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="/login"
+                            onClick={logout}
+                            to="/signup"
                             className="hidden sm:block text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log Out
                         </Link>
                         <Link
-                            to="/signup"
+                            to="/home"
                             className="hidden sm:block text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
