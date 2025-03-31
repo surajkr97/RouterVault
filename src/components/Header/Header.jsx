e07c8e2e5b3c5c0fd580a92eb968947438b79074
Suraj Kumar <surajkumar06174@gmail.com>
2025-03-31 21:03:44 +0530

    feat/ui-updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
                             to="/login"
                             className="hidden sm:block text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Log In
+                            Log Out
                         </Link>
                         <Link
                             to="/signup"
