884792c18b4848e1766a8058e395990dc3a07973
Suraj Kumar <surajkumar06174@gmail.com>
2025-04-01 23:45:02 +0530

    updated-user-params

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 const User = () => {
   const {userid} = useParams();
-  return <div className="flex justify-center text-2xl p-4 bg-orange-50">User: {userid}</div>;
+  return <div className="flex justify-center text-2xl p-45 bg-orange-50">User: {userid}</div>;
 };
 
 export default User;
